ae9f9e753802442554364191ab04a5d891cee1c3
Rizqi Mauludin <40969099+rizqimauludin10@users.noreply.github.com>
2019-06-19 01:03:49 +0700

    coomit 19/06/19 - 1.03

--- app/src/main/java/com/example/tugasakhir_nyuciapps/Search.java
+++ app/src/main/java/com/example/tugasakhir_nyuciapps/Search.java
@@ -2,7 +2,10 @@ package com.example.tugasakhir_nyuciapps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class Search extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class Search extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 }
