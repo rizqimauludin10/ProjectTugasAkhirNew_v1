a3879fb83f367206c0eb2e05382c7016eb7dfb18
Rizqi Mauludin <40969099+rizqimauludin10@users.noreply.github.com>
2019-06-22 00:52:36 +0700

    Login UI/Password UI

--- app/src/main/java/com/example/tugasakhir_nyuciapps/LoginActivity.java
+++ app/src/main/java/com/example/tugasakhir_nyuciapps/LoginActivity.java
@@ -4,15 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class LoginActivity extends AppCompatActivity {
 
+    ImageView showPassword;
+    EditText etpassword;
+
+    boolean password_status = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        showPassword = (ImageView) findViewById(R.id.show_password);
+        etpassword = (EditText) findViewById(R.id.et_password);
+
+        showPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (password_status) {
+                    password_status = false;
+                    etpassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                } else {
+                    password_status = true;
+                    etpassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+
+
+            }
+        });
     }
 
     @Override
